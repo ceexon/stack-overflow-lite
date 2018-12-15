c782233e15b7ae2b6d332d05b66e7c1b21e6fe57
Kburudi <bkevery@gmail.com>
2018-12-15 23:41:08 +0300

    [finishes #162683863]Added get all endpoint for user to access questions

---
+++ Api/api.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify, render_template
+from data.answers import Answers as ans
+from data.questions import Questions as quiz
+from data.userdata import Users as user
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<h1>It works</h1>"
+
+
+@app.route('/question', methods=['GET'])
+def det_all_questions():
+    asked = []
+    for i, question in enumerate(quiz):
+        numi = str(i+1)
+        asked.append({numi+'. ': question["description"]})
+
+    return jsonify({"All questions": asked})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
