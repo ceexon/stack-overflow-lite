2548104c6e852db98e263439ca62f3661e004d6a
Kburudi <bkevery@gmail.com>
2018-12-15 22:37:09 +0300

    [finishes #162682489]

---
+++ Api/data/userdata.py
@@ -0,0 +1,50 @@
+Users = [
+    {
+        "username": "terv-012",
+        "email": "trev21@gmail.com",
+        "password": "pAsS123#",
+        "gender": "Male"
+    },
+    {
+        "username": "nicole",
+        "email": "nics@gmail.com",
+        "password": "nicsON34$",
+        "gender": "Female"
+    },
+    {
+        "username": "joseph",
+        "email": "jnjugush@yahoo.com",
+        "password": "JjJj88eit#",
+        "gender": "Male"
+    },
+    {
+        "username": "marvin",
+        "email": "coolmav@outlook.com",
+        "password": "coolMav00!!",
+        "gender": "Male"
+    },
+    {
+        "username": "queenBee",
+        "email": "qbee@gmail.com",
+        "password": "QBqb56$#",
+        "gender": "Female"
+    },
+    {
+        "username": "mukeran",
+        "email": "mukeranj@hotmail.com",
+        "password": "jmKeRan@55$",
+        "gender": "Male"
+    },
+    {
+        "username": "stacy",
+        "email": "stacie@gmail.com",
+        "password": "6%2$saCY",
+        "gender": "Female"
+    },
+    {
+        "username": "kevo",
+        "email": "kelvin@gmail.com",
+        "password": "99##kkNN",
+        "gender": "Male"
+    }
+]
